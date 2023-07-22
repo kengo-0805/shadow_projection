de739744d73bf219bca854e53a33a561f8a576ff
kengoMBP <kengo0805ooo@gmail.com>
2023-07-23 05:00:17 +0900

    狼のサイズが可変でマウスコントロールも可能

--- cg_make/samplecode/opengl_2tex.py
+++ cg_make/samplecode/opengl_2tex.py
@@ -13,36 +13,36 @@ def resize_texture(texture, new_width, new_height):
     glBindTexture(GL_TEXTURE_2D, new_texture_id)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, new_width, new_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixel_data)
 
-    # Create a new TextureRegion using the new texture
-    new_texture = pyglet.image.Texture(new_width, new_height, texture.format, id=new_texture_id)
-    new_texture_region = new_texture.get_region(0, 0, new_width, new_height)
+    return new_texture_id
 
-    return new_texture_region
+def load_texture(filename):
+    image = Image.open(filename)
+    image_data = image.tobytes("raw", "RGB", 0, -1)
+    width, height = image.size
 
+    texture_id = GLuint(0)
+    glGenTextures(1, texture_id)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
+
+    return texture_id, width, height
 
 # Window dimensions
 WIDTH, HEIGHT = 1200, 1000
 
-tex2 = Image.open('data/wolf.png')
-width, height = 100, 100
-tex2_resized = tex2.resize((width, height))
 # Load textures
-texture1 = pyglet.image.load('data/back.JPG').get_texture()
-texture2 = pyglet.image.load('data/wolf.png').get_texture()
-
-# Resize texture
-# width = 10  # Desired width
-# height = 20  # Desired height
-# texture2 = texture2.get_region(0, 0, width, height)
-
-# Resize texture2 to new width and height
-new_width = 200
-new_height = 200
-texture2 = resize_texture(texture2, new_width, new_height)
-print(type(texture2))
+texture1_id, texture1_width, texture1_height = load_texture('data/back.JPG')
+texture2_id, texture2_width, texture2_height = load_texture('data/wolf.png')
 
 # Create vertex list for quad
-vertex_list = pyglet.graphics.vertex_list(4,
+vertex_list_back = pyglet.graphics.vertex_list(4,
+    ('v2f', [0, 0, WIDTH, 0, WIDTH, HEIGHT, 0, HEIGHT]),
+    ('t2f', [0, 0, 1, 0, 1, 1, 0, 1])
+)
+WIDTH, HEIGHT = 200, 200
+vertex_list_front = pyglet.graphics.vertex_list(4,
     ('v2f', [0, 0, WIDTH, 0, WIDTH, HEIGHT, 0, HEIGHT]),
     ('t2f', [0, 0, 1, 0, 1, 1, 0, 1])
 )
@@ -59,24 +59,33 @@ window = pyglet.window.Window(WIDTH, HEIGHT, resizable=True)
 @window.event
 def on_draw():
     window.clear()
-    
+
     glEnable(GL_TEXTURE_2D)
-    
+
     # Bind and draw texture1
-    glBindTexture(texture1.target, texture1.id)
+    glBindTexture(GL_TEXTURE_2D, texture1_id)
     glPushMatrix()
     glTranslatef(texture1_x, texture1_y, 0)
-    vertex_list.draw(GL_QUADS)
+    vertex_list_back.draw(GL_QUADS)
     glPopMatrix()
-    
+
     # Bind and draw texture2 overlaid on texture1
-    glBindTexture(texture2.target, texture2.id)
+    glBindTexture(GL_TEXTURE_2D, texture2_id)
     glPushMatrix()
     glTranslatef(texture2_x, texture2_y, 0)
-    vertex_list.draw(GL_QUADS)
+    vertex_list_front.draw(GL_QUADS)
     glPopMatrix()
 
     glDisable(GL_TEXTURE_2D)
+
+@window.event
+def on_resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, width, 0, height, -1, 1)
+    glMatrixMode(GL_MODELVIEW)
+    return pyglet.event.EVENT_HANDLED
 
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
